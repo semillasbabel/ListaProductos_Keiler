0dc3f121297846566059d280988b05507548cfe7
KeilerGB <keiler.cortes@grupobabel.com>
2022-08-19 13:55:07 -0600

    Proceso de Creación de UI

--- App.js
+++ App.js
@@ -1,20 +1,142 @@
-import * as React from 'react';
+import React, { useState } from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, Image, TextInput, View, Button} from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  Image, 
+  TextInput, 
+  View, 
+  Button,
+  FlatList,
+  SafeAreaView,
+  TouchableOpacity,
+} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const DATA = [
+  {
+    id: "1",
+    title: "First Item",
+  },
+  {
+    id: "2",
+    title: "Second Item",
+  },
+  {
+    id: "3",
+    title: "Third Item",
+  },
+  {
+    id: "4",
+    title: "First Item",
+  },
+  {
+    id: "5",
+    title: "Second Item",
+  },
+  {
+    id: "6",
+    title: "Third Item",
+  },
+  {
+    id: "7",
+    title: "First Item",
+  },
+  {
+    id: "8",
+    title: "Second Item",
+  },
+  {
+    id: "9",
+    title: "Third Item",
+  },
+  {
+    id: "10",
+    title: "First Item",
+  },
+  {
+    id: "11",
+    title: "Second Item",
+  },
+  {
+    id: "12",
+    title: "Third Item",
+  },
+];
+
+const Item = ({ item, onPress, backgroundColor, textColor }) => (
+  <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
+    <Image
+    source={{
+      uri: 'https://reactnative.dev/docs/assets/p_cat2.png',}}
+      style={{ width: 20, height: 20 }}/>
+    <Text style={[styles.title, textColor]}>{item.title}</Text>
+  </TouchableOpacity>
+);
+
+
 function HomeScreen({navigation}) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
+  const [selectedId, setSelectedId] = useState(null);
+
+  const renderItem = ({ item }) => {
+    const backgroundColor = item.id === selectedId ? "#86AD09" : "gray";
+    const color = item.id === selectedId ? 'white' : 'black';
+
+    return (
+      <Item
+        item={item}
+        // onPress={() => setSelectedId(item.id)}
         onPress={() => navigation.navigate('Details')}
+        backgroundColor={{ backgroundColor }}
+        textColor={{ color }}
       />
-    </View>
+    );
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        extraData={selectedId}
+      />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+    backgroundColor: "white",
+  },
+  item: {
+    flexDirection: 'row',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 20,
+  },
+  title: {
+    fontSize: 20,
+  },
+});
+
+
+
+// function HomeScreen({navigation}) {
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Text>Home Screen</Text>
+//       <Button
+//       title="Go to Details"
+//       onPress={() => navigation.navigate('Details')}/>
+//     </View>
+//   );
+// }
+
+
 function DetailsScreen({navigation}) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -31,12 +153,27 @@ function DetailsScreen({navigation}) {
     </View>
   );
 }
+
+
 const Stack = createNativeStackNavigator();
+
+
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} options={{title: 'Inicio'}} />
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#1168AD'},
+          headerTintColor: 'black',
+          headerTitleAlign: 'center',
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Lista Productos' }}
+        />
         <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
