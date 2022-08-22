d6fe11c7c3cbe6d484f0aa290e5b303e685bc6c2
KeilerGB <keiler.cortes@grupobabel.com>
2022-08-22 13:24:53 -0600

    Cambio en el nombre de los productos

--- ProyectData/Data/Products.js
+++ ProyectData/Data/Products.js
@@ -2,7 +2,7 @@ export const DATA = [
 
     {
       id: "1",
-      name: "HYUNDAI HYBOOK - CELERON N4020",
+      name: "HYUNDAI HYBOOK - CELERON N4020 - 4 GB RAM",
       price: "159,000",
       imageuri: "https://extremetechcr.com/tienda/19452-large_default/hyundai-hybook-celeron-n4020-4-gb-ram.jpg",
       description: "14 pulgadas - 4 Gb ram - SSD 128 Gb - Windows 10"
@@ -10,7 +10,7 @@ export const DATA = [
 
     {
       id: "2",
-      name: "TOSHIBA DYNABOOK TECRA A30-G",
+      name: "TOSHIBA DYNABOOK TECRA A30-G - INTEL CELERON 5205U - 4 GB - SSD",
       price: "165,000",
       imageuri: "https://extremetechcr.com/tienda/19998-large_default/toshiba-dynabook-tecra-a30-g-intel-celeron-5205u-4-gb-ssd.jpg",
       description: "intel Celeron 5205U - 4GB Ram - SSD - 128 GB - Windows 10"
@@ -18,7 +18,7 @@ export const DATA = [
 
     {
       id: "3",
-      name: "ACER ASPIRE 5",
+      name: "ACER ASPIRE 5 - CORE I3 10110U - 4 GB RAM",
       price: "234,000",
       imageuri: "https://extremetechcr.com/tienda/17986-large_default/acer-aspire-5-a515-54-core-i3-10110u-4-gb-ram-ssd-black.jpg",
       description: "Core i3 - 4 GB Ram - SSD 128 GB - Windows 10"
@@ -26,7 +26,7 @@ export const DATA = [
 
     {
       id: "4",
-      name: "LENOVO IDEAPAD 3",
+      name: "LENOVO IDEAPAD 3 - 81WE0016US - CORE I3 1005G1 - 4 GB RAM - SSD 128 GB",
       price: "275,000",
       imageuri: "https://extremetechcr.com/tienda/13928-large_default/lenovo-ideapad-3-81we0016us-core-i3-1005g1-4-gb-ram-ssd-128-gb.jpg",
       description: "Core i3 - 4 GB Ram - SSD 128 GB - Windows 10"
@@ -34,7 +34,7 @@ export const DATA = [
 
     {
       id: "5",
-      name: "LENOVO V15",
+      name: "LENOVO V15 - 82KB013HSP - I3 1115G4 - 8 GB - 256 GB SSD",
       price: "289,000",
       imageuri: "https://extremetechcr.com/tienda/21301-large_default/lenovo-v15-82kb013hsp-i3-1115g4-8-gb-256-gb-ssd-.jpg",
       description: "Core i3 - 8 GB Ram - SSD 256 GB - Windows 11"
@@ -42,7 +42,7 @@ export const DATA = [
 
     {
       id: "6",
-      name: "HP 14-DK1032WM",
+      name: "HP 14-DK1032WM - RYZEN 3 3250U - 4 GB - SSD",
       price: "295,000",
       imageuri: "https://extremetechcr.com/tienda/20406-large_default/hp-14-dk1032wm-ryzen-3-3250u-4-gb-125gb-ssd.jpg",
       description: "Ryzen 3 - 4 GB Ram - SSD 128 GB - Windows 10"
@@ -50,7 +50,7 @@ export const DATA = [
 
     {
       id: "7",
-      name: "HP 14-DQ2031TG",
+      name: "HP 14-DQ2031TG - CORE I3 1115G4 - 4 GB RAM",
       price: "295,000",
       imageuri: "https://extremetechcr.com/tienda/20407-large_default/hp-14-dq2031tg-i3-1115g4-4-gb-ram.jpg",
       description: "Core i3 - 4 GB Ram - SSD 128 GB - Windows 10"
@@ -58,7 +58,7 @@ export const DATA = [
 
     {
       id: "8",
-      name: "DELL INSPIRON 14",
+      name: "DELL INSPIRON 14 3493 - CORE I3 1005G1 - 4 GB RAM",
       price: "320,000",
       imageuri: "https://extremetechcr.com/tienda/16850-large_default/dell-inspiron-3493-core-i3-1005g1-4-gb-ram-128-ssd.jpg",
       description: "Core i3 - 4 GB Ram - SSD 128 GB - Windows 10"
@@ -66,7 +66,7 @@ export const DATA = [
 
     {
       id: "9",
-      name: "TOSHIBA DYNABOOK SATELLITE PRO C50-J",
+      name: "TOSHIBA DYNABOOK SATELLITE PRO C50-J - I3-1115G4 - 8 GB",
       price: "339,000",
       imageuri: "https://extremetechcr.com/tienda/20815-large_default/toshiba-dynabook-satellite-pro-c50-j-i3-1115g4-8-gb-ssd.jpg",
       description: "Core i3 - 8 GB Ram - SSD 256 GB - Windows 11"
@@ -74,7 +74,7 @@ export const DATA = [
 
     {
       id: "10",
-      name: "LENOVO IDEAPAD 3",
+      name: "LENOVO IDEAPAD 3 - 81WA00L4GJ - I3 10110U - 8 GB RAM",
       price: "339,000",
       imageuri: "https://extremetechcr.com/tienda/20507-large_default/lenovo-ideapad-3-81wa00l4gj-core-i3-10110u-8-gb-ram.jpg",
       description: "Core i3 - 8 GB Ram - SSD 256 GB - Windows 11"
